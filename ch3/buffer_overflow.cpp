5bc4bc670ebcd3d84f5d491b72bb94be64857a6a
bliutwo <bliutwo@cs.stanford.edu>
2020-04-26 22:59:33 -0700

    26/04/2020 22:59:33

--- ch3/buffer_overflow.cpp
+++ ch3/buffer_overflow.cpp
@@ -1,11 +1,20 @@
 #include <cstdio>
 
-void read_from(char s[]) {
-
+char read_from(char* s, int n, int i) {
+    if (i >= n || i < 0) {
+        printf("Invalid read.\n");
+        return '\0';
+    } else {
+        return s[i];
+    }
 }
 
-void write_to(char s[]) {
-
+void write_to(char* s, int n, int i, char c) {
+    if (i >= n || i < 0) {
+        printf("Invalid write.\n");
+    } else {
+        s[i] = c;
+    }
 }
 
 int main() {
@@ -13,13 +22,23 @@ int main() {
     char upper[] = "ABC?E";
     char* upper_ptr = upper;        // Equivalent: &upper[0]
 
-    lower[3] = 'd';                 // lower now contains a b c d e \0
-    upper_ptr[3] = 'D';             // upper now contains A B C D E \0
+    printf("%zu\n", sizeof(lower));
 
-    char letter_d = lower[3];       // letter_d equals 'd'
-    char letter_D = upper_ptr[3];   // letter_D equals 'D'
+    // lower[3] = 'd';                 // lower now contains a b c d e \0
+    // upper_ptr[3] = 'D';             // upper now contains A B C D E \0
+    
+    write_to(lower, sizeof(lower), 3, 'd');
+    write_to(upper, sizeof(upper), 3, 'D');
+
+    // char letter_d = lower[3];       // letter_d equals 'd'
+    // char letter_D = upper_ptr[3];   // letter_D equals 'D'
+    
+    char letter_d = read_from(lower, sizeof(lower), 3);
+    char letter_D = read_from(upper, sizeof(upper), 3);
 
     printf("lower: %s\nupper: %s\n", lower, upper);
 
-    lower[7] = 'g';                 // Super bad. You must never do this.
+    // lower[7] = 'g';                 // Super bad. You must never do this.
+    write_to(lower, sizeof(lower), 7, 'g');
+    write_to(upper, sizeof(upper), -2948357, 'f');
 }
--- ch3/linked_list.cpp
+++ ch3/linked_list.cpp
@@ -2,9 +2,14 @@
 
 struct Element {
     Element* next{};
+    Element* prev{};
     void insert_after(Element* new_element) {
         new_element->next = this->next;
         this->next = new_element;
+    }
+    void insert_before(Element* new_element) {
+        new_element->next = this;
+        this->prev = new_element;
     }
     char prefix[2];
     short operating_number;
@@ -15,16 +20,42 @@ int main() {
     trooper1.prefix[0] = 'T';
     trooper1.prefix[1] = 'K';
     trooper1.operating_number = 421;
-    trooper1.insert_after(&trooper2);
+    // trooper1.insert_after(&trooper2);
     trooper2.prefix[0] = 'F';
     trooper2.prefix[1] = 'N';
     trooper2.operating_number = 2187;
-    trooper2.insert_after(&trooper3);
+    // trooper2.insert_after(&trooper3);
+    trooper2.insert_before(&trooper1);
     trooper3.prefix[0] = 'L';
     trooper3.prefix[1] = 'S';
     trooper3.operating_number = 005;
+    trooper3.insert_before(&trooper2);
 
     for (Element *cursor = &trooper1; cursor; cursor = cursor->next) {
+        printf("stormtrooper %c%c-%d\n",
+                cursor->prefix[0],
+                cursor->prefix[1],
+                cursor->operating_number);
+        if (cursor->prev) {
+            printf("previous stormtrooper: %c%c-%d\n",
+                    cursor->prev->prefix[0],
+                    cursor->prev->prefix[1],
+                    cursor->prev->operating_number);
+        }
+        if (cursor->next) {
+            printf("next stormtrooper: %c%c-%d\n",
+                    cursor->next->prefix[0],
+                    cursor->next->prefix[1],
+                    cursor->next->operating_number);
+        }
+    }
+    for (Element *cursor = &trooper1; cursor; cursor = cursor->next) {
+        printf("stormtrooper %c%c-%d\n",
+                cursor->prefix[0],
+                cursor->prefix[1],
+                cursor->operating_number);
+    }
+    for (Element *cursor = &trooper3; cursor; cursor = cursor->prev) {
         printf("stormtrooper %c%c-%d\n",
                 cursor->prefix[0],
                 cursor->prefix[1],
