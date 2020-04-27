3f5566939b75157eb9e7e1c0e02bc58b5f64a6c7
bliutwo <bliutwo@cs.stanford.edu>
2020-04-27 12:45:54 -0700

    27/04/2020 12:45:54

--- ch4/groucho.cpp
+++ ch4/groucho.cpp
@@ -3,11 +3,20 @@
 
 struct Groucho {
     void forget(int x) {
-        if (x == OxFACE) {
-            throw std::runtime_error{ "I'd be glad to make an exception.\n"};
+        if (x == 0xFACE) {
+            throw std::runtime_error{ "I'd be glad to make an exception."};
         }
-        printf("Forgot Ox%x\n", x);
+        printf("Forgot 0x%x\n", x);
     }
 };
 
-
+int main() {
+    Groucho groucho;
+    try {
+        groucho.forget(0xC0DE);
+        groucho.forget(0xFACE);
+        groucho.forget(0xC0FFEE);
+    } catch (const std::runtime_error& e) {
+        printf("exception caught with message: %s\n", e.what());
+    }
+}
