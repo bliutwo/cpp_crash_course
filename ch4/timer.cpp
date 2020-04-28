54a014598ace257091d3bff66c51f0a5738abd59
bliutwo <bliutwo@cs.stanford.edu>
2020-04-27 23:57:11 -0700

    27/04/2020 23:57:11

--- ch4/timer.cpp
+++ ch4/timer.cpp
@@ -4,6 +4,13 @@
 struct TimerClass {
     TimerClass() {
         timestamp = time(0);
+        printf("Time: %s", ctime(&timestamp));
+    }
+    ~TimerClass() {
+        double seconds = difftime(time(0), timestamp);
+        // Goal: Get correct format specifier for double
+        // Link: https://stackoverflow.com/questions/4264127/correct-format-specifier-for-double-in-printf
+        printf("Age of timer: %f", seconds);
     }
 private:
     time_t timestamp;
