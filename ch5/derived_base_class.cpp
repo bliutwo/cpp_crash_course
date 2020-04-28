8792a2265ec4bb6a8c858c8675e7a8909a9c7dd6
bliutwo <bliutwo@cs.stanford.edu>
2020-04-28 15:17:46 -0700

    28/04/2020 15:17:46

--- ch5/derived_base_class.cpp
+++ ch5/derived_base_class.cpp
@@ -1,13 +1,20 @@
 #include <cstdio>
 
 struct BaseClass {
-    int the_answer() const { return 42; }
-    const char* member = "gold";
-private:
-    const char* holistic_detective = "Dirk Gently";
+//    int the_answer() const { return 42; }
+//    const char* member = "gold";
+//private:
+//    const char* holistic_detective = "Dirk Gently";
+    virtual const char* final_message() const {
+        return "We apologize for the incontinence.";
+    }
 };
 
-struct DerivedClass : BaseClass {};
+struct DerivedClass : BaseClass {
+    const char* final_message() const override {
+        return "We apologize for the inconvenience.";
+    }
+};
 void are_belong_to_us(BaseClass& base) {}
 void all_about_that(DerivedClass& derived) {}
 
@@ -23,11 +30,21 @@ int main2() {
     return 0;
 }
 
+//int main3() {
+//    DerivedClass x;
+//    printf("The answer is %d\n", x.the_answer());
+//    printf("%s member\n", x.member);
+//    // This line doesn't compile
+//    // printf("%s's Holistic Detective Agency\n", x.holistic_detective);
+//    return 0;
+//}
+
 int main() {
-    DerivedClass x;
-    printf("The answer is %d\n", x.the_answer());
-    printf("%s member\n", x.member);
-    // This line doesn't compile
-    // printf("%s's Holistic Detective Agency\n", x.holistic_detective);
+    BaseClass base;
+    DerivedClass derived;
+    BaseClass& ref = derived;
+    printf("BaseClass:    %s\n", base.final_message());
+    printf("DerivedClass: %s\n", derived.final_message());
+    printf("BaseClass&:   %s\n", ref.final_message());
     return 0;
 }
