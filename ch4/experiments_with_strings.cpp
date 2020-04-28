f43032d972aa68d4546519379aaa061063d86388
bliutwo <bliutwo@cs.stanford.edu>
2020-04-28 11:29:43 -0700

    28/04/2020 11:29:43

---
+++ ch4/experiments_with_strings.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+    const char* a = "wtf";
+    printf("%s has length %zu\n", a, strlen(a));
+    return 0;
+}
--- ch4/timer.cpp
+++ ch4/timer.cpp
@@ -1,22 +1,58 @@
 #include <cstdio>
 #include <ctime>
+#include <cstring>
 
 struct TimerClass {
-    TimerClass() {
+    TimerClass(const char* name) {
         timestamp = time(0);
+        this->name = new char[strlen(name)];
+        strncpy(this->name, name, strlen(name));
+    }
+    
+    TimerClass(const TimerClass& other) {
+        timestamp = other.timestamp;
+    }
+
+    TimerClass& operator=(const TimerClass& other) {
+        if (this == &other) return *this;
+        timestamp = other.timestamp;
+        return *this;
+    }
+
+    TimerClass(TimerClass&& other) noexcept {
+        timestamp = other.timestamp;
+        // Potential problem here because I don't know how to "zero out"
+        // the `other` TimerClass, since it's in a "moved-from" state
+        other.timestamp = 0;
+    }
+
+    TimerClass& operator=(TimerClass&& other) noexcept {
+        if (this == &other) return *this;
+        timestamp = other.timestamp;
+        // Same problem with this line
+        other.timestamp = 0;
+        return *this;
+    }
+
+    void get_time() {
         printf("Time: %s", ctime(&timestamp));
     }
+
     ~TimerClass() {
         double seconds = difftime(time(0), timestamp);
         // Goal: Get correct format specifier for double
         // Link: https://stackoverflow.com/questions/4264127/correct-format-specifier-for-double-in-printf
-        printf("Age of timer: %f", seconds);
+        printf("Age of timer %s: %f\n", name, seconds);
     }
 private:
     time_t timestamp;
+    char* name;
 };
 
 int main() {
-    TimerClass t = TimerClass();
+    TimerClass t = TimerClass("t");
+    t.get_time();
+    TimerClass s = t;
+    s.get_time();
     return 0;
 }
