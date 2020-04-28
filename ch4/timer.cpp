4be2bd47ec9ca902806cc60694b1c6e837b59d77
bliutwo <bliutwo@cs.stanford.edu>
2020-04-27 23:43:50 -0700

    27/04/2020 23:43:50

--- ch4/timer.cpp
+++ ch4/timer.cpp
@@ -1,18 +1,15 @@
 #include <cstdio>
 #include <ctime>
-#include <chrono>
-#include <iostream>
-#include <typeinfo>
 
-// struct TimerClass {
-//     TimerClass() {
-//     }
-// private:
-//     time_t timestamp;
-// };
+struct TimerClass {
+    TimerClass() {
+        timestamp = time(0);
+    }
+private:
+    time_t timestamp;
+};
 
 int main() {
-    auto start = std::chrono::system_clock::now();
-    std::cout << "start is: " << typeid(start).name() << std::endl;
+    TimerClass t = TimerClass();
     return 0;
 }
