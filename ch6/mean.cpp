849829a8168a13c6e6c15bca4cf6ccc8ba10dbe2
bliutwo <bliutwo@cs.stanford.edu>
2020-04-29 14:37:12 -0700

    Refactor mean.cpp to only accept array

--- ch6/mean.cpp
+++ ch6/mean.cpp
@@ -1,26 +1,26 @@
 #include <cstddef>
 #include <cstdio>
 
-template<typename T>
-T mean(const T* values, size_t length) {
+template<typename T, size_t Length>
+T mean(const T (&values)[Length]) {
     T result{};
-    for (size_t i{}; i < length; i++) {
+    for (size_t i{}; i < Length; i++) {
         result += values[i];
     }
-    return result / length;
+    return result / Length;
 }
 
 int main() {
     const double nums_d[] = {1.0, 2.0, 3.0, 4.0};
-    const auto result1 = mean(nums_d, 4);
+    const auto result1 = mean(nums_d);
     printf("double: %f\n", result1);
 
     const float nums_f[] {1.0f, 2.0f, 3.0f, 4.0f};
-    const auto result2 = mean(nums_f, 4);
+    const auto result2 = mean(nums_f);
     printf("float: %f\n", result2);
 
     const size_t nums_c[] {1, 2, 3, 4};
-    const auto result3 = mean(nums_c, 4);
+    const auto result3 = mean(nums_c);
     printf("size_t: %zd\n", result3);
 
     return 0;
