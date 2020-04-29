539b959e9ec93104ec6bc8114d77e508b538bf6c
bliutwo <bliutwo@cs.stanford.edu>
2020-04-29 13:03:18 -0700

    29/04/2020 13:03:18

---
+++ ch6/mode.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <typeinfo>
+
+int get_max_key(std::map<int, int> m) {
+    auto max_key = std::max_element(m.begin(), m.end(),
+            [](const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+                return p1.second < p2.second;
+            });
+    return max_key->first;
+}
+
+int mode(const int* values, size_t length) {
+    std::map<int, int> m;
+    for (size_t i{}; i < length; i++) {
+        int curr = values[i];
+        if ( m.find(curr) == m.end() ) {
+            m[curr] = 1;
+        } else {
+            m[curr] += 1;
+        }
+    }
+    return get_max_key(m);
+}
+
+int main() {
+    int arr[] = {1, 1, 2, 3, 4, 4, 4, 5};
+    // expect 4
+    printf("mode: %d\n", mode(arr, sizeof(arr) / sizeof(int)));
+    return 0;
+}
