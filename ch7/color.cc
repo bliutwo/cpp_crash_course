e3f00237c6043977dadab777e15d4afa6739822c
bliutwo <bliutwo@cs.stanford.edu>
2020-04-30 11:12:55 -0700

    30/04/2020 11:12:55

--- ch7/color.cc
+++ ch7/color.cc
@@ -1,5 +1,12 @@
+#include <cstdint>
+
 struct Color {
     float H, S, V;
 };
 
-
+constexpr uint8_t max(uint8_t a, uint8_t b) {
+    return a > b ? a : b;
+}
+constexpr uint8_t min(uint8_t a, uint8_t b, uint8_t c) {
+    return max(max(a, b), max(a, c));
+}
