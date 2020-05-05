c50e7de498718fcb08fd4334444853cd4b4c83f6
bliutwo <bliutwo@cs.stanford.edu>
2020-05-04 20:28:55 -0700

    04/05/2020 20:28:55

--- ch9/fold.cc
+++ ch9/fold.cc
@@ -1,11 +1,29 @@
 #include <cstdio>
+#include <limits>
 
 template <typename Fn, typename In, typename Out>
-constexpr Out fold(Fn function, In* input, size_t length, Out initial);
+constexpr Out fold(Fn function, In* input, size_t length, Out initial)
+{
+    if (length == 0) {
+        return initial;
+    }
+    Out last_element = input[length - 1];
+    return function(initial, fold(function, input, length - 1, last_element));
+}
 
 int main() {
     int data[]{ 100, 200, 300, 400, 500 };
     size_t data_len = 5;
+
     auto sum = fold([](auto x, auto y) { return x + y; }, data, data_len, 0);
     printf("Sum: %d\n", sum);
+
+    int inf = std::numeric_limits<int>::infinity();
+    auto max = fold([](auto x, auto y) { return x > y ? x : y; }, data, data_len, -1 * inf);
+    printf("Max: %d\n", max);
+
+    auto min = fold([](auto x, auto y) { return x < y ? x : y; }, data, data_len, inf);
+    printf("Min: %d\n", min);
+
+    return 0;
 }
--- ch9/lambda.cc
+++ ch9/lambda.cc
@@ -4,18 +4,18 @@
 #include <cstdint>
 
 template <typename Fn>
-void transform(Fn fn, const int* in, int* out, size_t length) { // 1
+void transform(Fn fn, const int* in, int* out, size_t length) {
     for (size_t i{}; i < length; i++) {
-        out[i] = fn(in[i]); // 2
+        out[i] = fn(in[i]);
     }
 }
 
 int main() {
     const size_t len{ 3 };
     int base[]{ 1, 2, 3 }, a[len], b[len], c[len];
-    transform([](int x) { return 1; } /*3*/, base, a, len);
-    transform([](int x) { return x; } /*4*/, base, b, len);
-    transform([](int x) { return 10*x+5; } /*5*/, base, c, len);
+    transform([](int x) { return 1; }, base, a, len);
+    transform([](int x) { return x; }, base, b, len);
+    transform([](int x) { return 10*x+5; }, base, c, len);
     for (size_t i{}; i < len; i++) {
         printf("Element %zd: %d %d %d\n", i, a[i], b[i], c[i]);
     }
