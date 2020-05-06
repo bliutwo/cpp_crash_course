9358ecc50fc22b0a57d5593f1e6c256643ee971b
bliutwo <bliutwo@cs.stanford.edu>
2020-05-06 09:55:40 -0700

    06/05/2020 09:55:40

--- ch9/length_histogram.cc
+++ ch9/length_histogram.cc
@@ -2,16 +2,16 @@
 #include <cstdint>
 
 struct LengthHistogram {
-    LengthHistogram();
+    LengthHistogram(int argc);
     ~LengthHistogram();
-    void ingest(const char* x);
-    void print() const;
+    void ingest(const char* x, int i);
+    void print(int argc) const;
 private:
     size_t* counts{};
 };
 
 LengthHistogram::LengthHistogram(int argc) {
-    counts = new size_t[argc];
+    counts = new size_t[argc]{};
 }
 
 LengthHistogram::~LengthHistogram() {
@@ -21,15 +21,15 @@ LengthHistogram::~LengthHistogram() {
 void LengthHistogram::ingest(const char* x, int i) {
     size_t index{};
     while (const auto c = x[index]) {
-        // TODO: here
+        counts[i]++;
         index++;
     }
 }
 
-void LengthHistogram::print() const {
-    for (size_t index{}; index <= pos_Z; index++) {
-        printf("%c: ", index);
-        auto n_asterisks = counts[index - pos_A];
+void LengthHistogram::print(int argc) const {
+    for (size_t index{1}; index < argc; index++) {
+        printf("%d:\t", index);
+        auto n_asterisks = counts[index];
         while (n_asterisks--) printf("*");
         printf("\n");
     }
@@ -40,6 +40,6 @@ int main(int argc, char** argv) {
     for (size_t i{ 1 }; i < argc; i++) { // 1
         hist.ingest(argv[i], i); // 2
     }
-    hist.print(); // 3
+    hist.print(argc); // 3
     return 0;
 }
