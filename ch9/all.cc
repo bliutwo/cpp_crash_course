a53a4ca80839e8716c040c92005bfcdefa2361e0
bliutwo <bliutwo@cs.stanford.edu>
2020-05-06 10:05:38 -0700

    06/05/2020 10:05:38

---
+++ ch9/all.cc
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+template <typename Fn, typename In>
+constexpr bool all(Fn function, In* input, size_t length);
+
+int main() {
+    int data[]{ /*100,*/ 200, 300, 400, 500 };
+    size_t data_len = 5;
+    auto all_gt100 = all([](auto x){ return x > 100; }, data, data_len);
+    if(all_gt100) printf("All elements greater than 100.\n");
+    return 0;
+}
+
+template <typename Fn, typename In>
+constexpr bool all(Fn function, In* input, size_t length) {
+    for (size_t i{}; i < length; i++)
+    {
+        if (!function(input[i])) {
+            return false;
+        }
+    }
+    return true;
+}
