990f163b94886a55e0646a469d77dcb553b66ed1
bliutwo <bliutwo@cs.stanford.edu>
2020-05-07 19:12:52 -0700

    07/05/2020 19:12:52

--- ch10/google_test.cc
+++ ch10/google_test.cc
@@ -1,3 +1,12 @@
+// NOTE: The commands for running this shit is the following:
+//
+// cmake .
+//
+// make
+//
+// That's it! I wish somewhere it was said to do that. Glad I finally
+// figured it out, though.
+
 #include "gtest/gtest.h"
 
 TEST(AutoBrake, UnitTestName) {
