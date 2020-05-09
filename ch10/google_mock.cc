b0643dd3b61fadeb535be2c174bf5c483a5a71c1
bliutwo <bliutwo@cs.stanford.edu>
2020-05-09 11:51:09 -0700

    09/05/2020 11:51:09

---
+++ ch10/google_mock.cc
@@ -0,0 +1,10 @@
+// Adding Google Mock to a third-party unit-testing framework
+
+#include "gmock/gmock.h"
+
+int main(int argc, char** argv) {
+    ::testing::GTEST_FLAG(throw_on_failure) = true;
+    ::testing::InitGoogleMock(&argc, argv);
+    // Unit test as usual, Google Mock is initialized
+    printf("Wow\n");
+}
